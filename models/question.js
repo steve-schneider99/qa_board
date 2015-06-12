be798098024afcabe8a80c63fd6bcfc2dcc3ba34
Steve <steve.schneider99@gmail.com>
2015-06-12 12:39:51 -0700

    add template code, updated some routes and controllers for refactoring, removed unused files.

--- models/question.js
+++ models/question.js
@@ -2,5 +2,5 @@ QaBoard.Question = DS.Model.extend({
   question: DS.attr(),
   author: DS.attr(),
   description: DS.attr(),
-  answers: DS.HasMany('answers', {async: true})
+  answers: DS.attr()
 });
--- router.js
+++ router.js
@@ -1,6 +1,4 @@
 QaBoard.Router.map(function() {
   this.resource('questions', {path: '/'});
-  this.resource('question', {path: ':question_id'}, function() {
-    this.resource('answers');
-  });
+  this.resource('question', {path: ':question_id'})
 });
